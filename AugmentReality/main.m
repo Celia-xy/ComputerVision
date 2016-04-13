ef4bd67691777b59a38b21a6bb6c864bfcbba3c4
Celia-xy <yaooxii@gmail.com>
2016-04-13 17:45:21 -0400

    improved part2

--- AugmentReality/main.m
+++ AugmentReality/main.m
@@ -144,3 +144,110 @@ R_0
 R
 test1
 test2
+
+%------------------- improving accuracy -------------------%
+% ---------- get approximate grid corners
+P_80 = 0.03*[0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9;
+    0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1, 2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,3,3, 4,4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5,5, 6,6,6,6,6,6,6,6,6,6, 7,7,7,7,7,7,7,7,7,7];
+P_80(3,:) = ones(1,80);
+p_approx = zeros(3,80,4);
+figure; 
+for i = 1 : 4
+    p_approx(:,:,i) = H(:,:,i)*P_80;
+    p_approx(:,:,i) = p_approx(:,:,i)./[p_approx(3,:,i);p_approx(3,:,i);p_approx(3,:,i)];
+    subplot(2,2,i); imshow(eval(['I',int2str(i)])); 
+    hold on;
+    c = uint16(p_approx(1,:,i));
+    r = uint16(p_approx(2,:,i));
+    plot(c,r,'go');
+    title(['Figure1:Projected grid corners ',int2str(i)]);
+end
+
+% ---------- detect Harris corners
+figure; 
+for i = 1 : 4     
+    [cim, r, c, rsubp, csubp] = harris(rgb2gray(eval(['I',int2str(i)])), 2, 500, 2, 0);
+    subplot(2,2,i); imshow(eval(['I',int2str(i)])); 
+    hold on;
+    plot(c,r,'rx');
+    title(['Figure2:Harris corners ',int2str(i)]);
+    
+    % -------- compute p_correct  
+    N = dist2([csubp,rsubp], p_approx(1:2,:,i)');
+    for j = 1 : 80
+        Ni = N(:,j);
+        n = find(Ni == min(Ni));
+        p_correct(1,j,i) = c(n);
+        p_correct(2,j,i) = r(n);
+    end
+end
+% print p_correct
+figure;
+for i = 1 : 4
+    subplot(2,2,i); imshow(eval(['I',int2str(i)])); 
+    hold on;
+    plot(p_correct(1,:,i),p_correct(2,:,i),'rx');
+    title(['Figure3:grid points ',int2str(i)]);
+end
+p_correct(3,:,:) = ones(1,80,4);
+
+% -------- compute new homography H
+for i = 1 : 4
+    h = homography2d(P_80, p_correct(:,:,i));
+    s = h*P_80;
+    H_new(:,:,i) = h;
+    v11 = [h(1,1)*h(1,1),h(1,1)*h(2,1)+h(2,1)*h(1,1),h(2,1)*h(2,1),h(3,1)*h(1,1)+h(1,1)*h(3,1),h(3,1)*h(2,1)+h(2,1)*h(3,1),h(3,1)*h(3,1)]';
+    v12 = [h(1,1)*h(1,2),h(1,1)*h(2,2)+h(2,1)*h(1,2),h(2,1)*h(2,2),h(3,1)*h(1,2)+h(1,1)*h(3,2),h(3,1)*h(2,2)+h(2,1)*h(3,2),h(3,1)*h(3,2)]';
+    v22 = [h(1,2)*h(1,2),h(1,2)*h(2,2)+h(2,2)*h(1,2),h(2,2)*h(2,2),h(3,2)*h(1,2)+h(1,2)*h(3,2),h(3,2)*h(2,2)+h(2,2)*h(3,2),h(3,2)*h(3,2)]';
+    V_0_new(2*i-1,:) = v12';
+    V_0_new(2*i,:) = v11'-v22';    
+end
+H_new
+% ---------- compute new K, R, t
+% compute new B
+[~,~,V] = svd(V_0_new);
+B_new = zeros(3,3);
+B_new(1,1) = V(1,6);
+B_new(1,2) = V(2,6);
+B_new(2,1) = V(2,6);
+B_new(2,2) = V(3,6);
+B_new(1,3) = V(4,6);
+B_new(3,1) = V(4,6);
+B_new(2,3) = V(5,6);
+B_new(3,2) = V(5,6);
+B_new(3,3) = V(6,6);
+% compute new A (or K)
+v_0_2d_new = (B_new(1,2)*B_new(1,3)-B_new(1,1)*B_new(2,3))/(B_new(1,1)*B_new(2,2)-B_new(1,2)*B_new(1,2));
+lambda_2d_new = B_new(3,3)-(B_new(1,3)*B_new(1,3)+v_0_2d_new*(B_new(1,2)*B_new(1,3)-B_new(1,1)*B_new(2,3)))/B_new(1,1);
+alpha_2d_new = sqrt(lambda_2d_new/B_new(1,1));
+beta_2d_new = sqrt(lambda_2d_new*B_new(1,1)/(B_new(1,1)*B_new(2,2)-B_new(1,2)*B_new(1,2)));
+gamma_2d_new = -B_new(1,2)*alpha_2d_new*alpha_2d_new*beta_2d_new/lambda_2d_new;
+u_0_2d_new = gamma_2d_new*v_0_2d_new/alpha_2d_new - B_new(1,3)*alpha_2d_new*alpha_2d_new/gamma_2d_new;
+A_new = [alpha_2d_new,gamma_2d_new,u_0_2d_new;0,beta_2d_new,v_0_2d_new;0,0,1];
+A_inv = eye(3)/A_new;
+R_new = zeros(3,3,4);
+t_new = zeros(3,4);
+% computer R and t
+ for i = 1:4  
+    R_new(:,:,i) = A_inv * H_new(:,:,i);
+    t_new(:,i) = R_new(:,3,i);
+    R_new(:,3,i) = cross(R_new(:,1,i),R_new(:,2,i));
+
+ end
+
+% print new R, A and t
+t_new
+R_new
+A_new
+
+% ----------------- compute error -----------------
+p_projected = zeros(3,80,4);
+for i = 1 : 4
+    % get projected corners
+    p_projected(:,:,i) = H_new(:,:,i)*P_80;
+    p_projected(:,:,i) = p_projected(:,:,i)./[p_projected(3,:,i);p_projected(3,:,i);p_projected(3,:,i)];
+    % compute previous error
+    err_previous(:,i) = (p_approx(1,:,i) - p_correct(1,:,i))*(p_approx(1,:,i) - p_correct(1,:,i))' + (p_approx(2,:,i) - p_correct(2,:,i))*(p_approx(2,:,i) - p_correct(2,:,i))';
+    % compute improved error
+    err_reprojection(:,i) = (p_projected(1,:,i) - p_correct(1,:,i))*(p_projected(1,:,i) - p_correct(1,:,i))' + (p_projected(2,:,i) - p_correct(2,:,i))*(p_projected(2,:,i) - p_correct(2,:,i))';
+end
